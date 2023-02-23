f2bdd279b2af9e2a958085c385e85731b86cba11
Paul Walker <paulwalker@paulwalker.dev>
2023-02-23 13:39:37 -0500

    Make gravity only work on non-stationary physics objects

--- Physics/Tasks/Gravity.c
+++ Physics/Tasks/Gravity.c
@@ -1,12 +1,17 @@
 #include "Gravity.h"
 #include "EngineMantle.h"
 
+#include "../Components/Physics.h"
 #include "../Components/Velocity.h"
 
 void t_gravity(Store *s, List *cd, List *e)
 {
+    TASK_CD(cd, Physics, cd_physics);
     TASK_CD(cd, Velocity, cd_velocity);
     
+    if (cd_physics->stationary)
+        return;
+
     cd_velocity->vy += 0.5;
     if (cd_velocity->vy < -8)
         cd_velocity->vy = -8;
